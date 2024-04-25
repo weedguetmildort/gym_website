64049b70484b1ab7f964f98c732f5ac70c54f0a8
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-04-25 15:54:34 -0400

    BenefitsPage Motion

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -107,19 +107,37 @@ const Benefits = ( { setSelectedPage }: Props) => {
                                 before:-left-20 before:z-[1] 
                                 before:content-abstractwaves"
                         >
-                            <div>
+                            <motion.div
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once:true, amount:0.5 }}
+                                transition={{ duration: 0.5 }}
+                                variants={{
+                                    hidden: {opacity: 0, x: 50},
+                                    visible: {opacity: 1, x: 0}
+                                }}  
+                            >
                                 <HText>
                                     MILLIONS OF HAPPY MEMBERS GETTING{" "}
                                     <span className="text-primary-500">FIT</span>
                                 </HText>
-                            </div>
+                            </motion.div>
 
                         </div>
 
                     </div>
 
                     {/*Description*/}
-                    <div>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once:true, amount:0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: {opacity: 0, x: -50},
+                            visible: {opacity: 1, x: 0}
+                        }}  
+                    >
                         <p className="my-5">
                             Nascetur aenean massa auctor tincidunt. Iaculis 
                             potenti amet egestas ultrices consectetur adipiscing
@@ -136,7 +154,7 @@ const Benefits = ( { setSelectedPage }: Props) => {
                             tortor facilisis metus nibh. Rhoncus sit enim mattis
                             odio in risus nunc.
                         </p>
-                    </div>
+                    </motion.div>
 
                     {/*Button*/}
                     <div className="relative mt-16">
