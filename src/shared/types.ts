ca7c7f082b7838c74017360d075a1a04945a2ee6
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-04-25 15:50:22 -0400

    BenefitsPage Update

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,9 @@ export enum SlctdPage {
     OurClasses = "ourclasses",
     ContactUs = "contactus"
   }
+
+  export interface BenefitType {
+    icon: JSX.Element;
+    title: string;
+    description: string;
+  }
