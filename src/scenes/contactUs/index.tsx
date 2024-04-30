a43ebeed8fa84aff0b837d135b6a73b38d989d1f
Weedguet Mildort <108565362+weedguetmildort@users.noreply.github.com>
2024-04-30 16:51:34 -0400

    FooterPage Setup

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const ContactUs = ({setSelectedPage}: Props) => {
 
-    const inputStyles = `w-full rounded-lg bg-primary-300 px-5 
+    const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 
     p-3 placeholder-white`;
 
     const {
@@ -106,10 +106,60 @@ const ContactUs = ({setSelectedPage}: Props) => {
                             </p>
                         )}
 
+                        <textarea
+                            className={inputStyles}
+                            rows={4}
+                            cols={50}
+                            placeholder="MESSAGE"
+                            {...register("message", {
+                            required: true,
+                            maxLength: 2000
+                            })}
+                        />
+                        
+                        {errors.message && (
+                            <p className="mt-1 text-primary-500">
+                                {errors.message.type === "required" &&
+                                    "This field is required."}
+                                {errors.message.type === "maxLength" && "Max length is 2000 char."}
+                            </p>
+                        )}
+
+                        <button
+                            type="submit"
+                            className="mt-5 rounded-lg bg-secondary-500 px-20
+                                py-3 transition duration-500 hover:text-white"
+                        >
+                            SUBMIT
+                        </button>
                     </form>
+                </motion.div>
+                
+                <motion.div
+                    className="relative mt-16 basis-2/5 md:mt-0"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once:true, amount:0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: {opacity: 0, y: 50},
+                        visible: {opacity: 1, y: 0}
+                    }}  
+                >
+                    <div
+                        className="md:before:content-evolvetext w-full 
+                            before:absolute before:-bottom-20 before:-right-10
+                            before:z-[-1]"
+                    >
+                        <img 
+                            className="w-full"
+                            alt="contact-us-page-graphic"
+                            src={ContactUsPageGraphic}
+                        />
+                    </div>
+
 
                 </motion.div>
-
             </div>
         </motion.div>
 
